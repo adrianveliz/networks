317979f2f18a7c3f58bfacec18b26bed5e77a4aa
Adrian Veliz <adrianveliz@live.com>
2016-06-08 21:55:55 -0600

    Implemented main

--- SimpleProxy.go
+++ SimpleProxy.go
@@ -1,1 +1,46 @@
+package main
 
+import (
+	"net"
+	"fmt"
+	)
+
+type Proxy struct{
+	from *net.Conn
+	to *net.Conn
+}
+
+func (p Proxy)Forward(msg chan string){
+	fmt.Print("got here")
+	close(msg)
+}
+
+
+func main(){
+	
+	fmt.Print("started\n")
+	msg1 := make(chan string)
+	msg2 := make(chan string)
+	
+	ln, err := net.Listen("tcp", ":5000")
+	if err != nil {
+		// handle error
+	}
+	
+	conn1, err := ln.Accept()
+	if err != nil {
+		// handle error
+	}
+	
+	conn2, err := net.Dial("tcp", ":5001")
+	if err != nil {
+		// handle error
+	}
+	
+	cToS := Proxy{&conn2, &conn1}
+	sToC := Proxy{&conn1, &conn2}
+	
+	
+	go cToS.Forward(msg1)
+	go sToC.Forward(msg2)
+}
